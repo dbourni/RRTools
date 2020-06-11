046f381a78ac488e7fc7fa39b0b08ad526e51813
David Bournisien <david@bournisien.net>
2020-06-11 14:38:05 +0200

    Récupération des infos grace à l'API RR

--- RRTools/MainForm.cs
+++ RRTools/MainForm.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +17,8 @@ namespace RRTools
 {
     public partial class MainForm : Form
     {
+        static HttpClient client = new HttpClient();
+
         public MainForm()
         {
             InitializeComponent();
@@ -21,5 +28,36 @@ namespace RRTools
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String data = Get_request("https://api.raceresult.com/153860/324CV6Q778H7G5VZ8IIR5W7RAFSVBJL7");
+            if (data !="error")
+            {
+                MessageBox.Show(data);
+            }
+
+        }
+
+        private string Get_request(string Url)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+                request.ContentType = "application/json; charset=utf-8";
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    return reader.ReadToEnd();
+                }
+            }
+            catch
+            {
+                return "error";
+            }
+
+        }
+
     }
 }
